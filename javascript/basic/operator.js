31cd8f7f7161beeccded49a1af24442236643687
Aniket Kharga <aniketkharga91@gmail.com>
2024-04-23 08:42:12 +0545

    ans added

--- javascript/basic/operator.js
+++ javascript/basic/operator.js
@@ -4,9 +4,11 @@ const x = 10;
 const y = 20;
 const z= x+y
 console.log(z)
+// ans = 30
 
 
 //Check below code, output should be false by comparing. console.log the answer
 const a = 10
 const b = '10'
 console.log(a===b)
+// ans= false
