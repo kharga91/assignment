d00273b3ed15191806738db7e8f65c2c6fef069b
Aniket Kharga <aniketkharga91@gmail.com>
2024-04-23 09:16:37 +0545

    array solved

---
+++ javascript/array/level1.js
@@ -0,0 +1,20 @@
+const arr = [3,5,2]
+//Q1 calculate length of array and console log
+//expected output is 3
+
+console.log(arr.length)
+//ans = 3
+
+
+//Q2 reverse the array and console log
+//expected output [2,5,3]
+const reversed = arr.reverse();
+console.log('reversed:', reversed);
+//ans reversed: [ 2, 5, 3 ]
+
+
+///Q3 join the array and console log
+// expected output 352
+
+console.log(arr.join(''));
+//ans 253
