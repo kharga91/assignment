d47835122659707c86c113e1150786ae9ee47fa9
Aniket Kharga <aniketkharga91@gmail.com>
2024-04-24 23:25:01 +0545

    all done

--- javascript/array/level1.js
+++ javascript/array/level1.js
@@ -19,3 +19,35 @@ console.log(arr.reverse())
 
 console.log(arr.join(''));
 //ans 253
+
+
+//Q4 Remove the last element from the array and log the modified array to the console
+
+const color = ['blue', 'Purple', 'red', 'black', 'white'];
+
+console.log(color.pop());
+// ans = white
+console.log(color);
+// ans= [ 'blue', 'Purple', 'red', 'black' ]
+
+
+
+// Q5 Check if all elements in the num  are even. Log "All even" or "Not all even" accordingly.
+const num = [6,4,4,8,10,12]
+
+if (num/2){
+  console.log('All Even');
+}else{
+  console.log('Not all even');
+}
+//ans = not all even
+
+
+
+
+
+// Find the index of a specific element in the array. Log the index to the console. 
+const paint = ['blue', 'Purple', 'red', 'black', 'white'];
+
+console.log(paint.indexOf('Purple'));
+//and =1
