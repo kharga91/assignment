32b139d0986bd7a09dc4aab37cc0cc87e583c44d
Aniket Kharga <aniketkharga91@gmail.com>
2024-04-24 23:57:18 +0545

    done

--- javascript/array/level2.js
+++ javascript/array/level2.js
@@ -4,3 +4,30 @@ const arr =  [3,5,2]
 
 console.log(Math.max(3, 5, 2));
 //ans 5
+
+//Q2 Remove a specific element 2 from the array. Log the modified array to the console.
+// expected output: [3,5]
+
+
+arr.splice(2,2);
+console.log(arr)
+//ans= [ 3, 5 ]
+
+
+
+
+
+//Q3 Multiply each element of the array by a constant value 5 and log the modified array to the console.
+//expected output [15,25]
+
+
+const multiply = arr.map(function(arr) {
+	return arr*5;
+});
+console.log(multiply);
+//ans =[ 15, 25 ]
+
+//find intersection (common elements) of arr and arr2
+//expected output: [5]
+const arr2 = [5,21,32]
+
