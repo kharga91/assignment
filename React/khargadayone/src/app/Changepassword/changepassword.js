c09bcc642b44c7f5a13a476244f68b680d88ef52
Aniket Kharga <aniketkharga91@gmail.com>
2024-04-29 15:55:01 +0545

    login page

---
+++ React/khargadayone/src/app/Changepassword/changepassword.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const changepassword = () => {
+  return (
+    <div>
+      <input placeholder='enter name' />
+      <input placeholder='enter password' />
+      <input placeholder='enter address' />
+      <input placeholder='enter Hone' />
+    </div>
+  )
+}
+
+export default changepassword
---
+++ React/khargadayone/src/app/page.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Home = () => {
+
+  return (
+    <div>
+      hi
+      <button>Click me</button>
+      <input placeholder="Enter your name" />
+      <li>apple</li>
+      <li>ball</li>
+      <li>cat</li>
+      <div className="greet" >
+             Hello React
+      </div>
+      
+    </div>
+    
+    
+  )
+}
+export default Home
